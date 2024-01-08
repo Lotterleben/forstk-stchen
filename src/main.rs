4ee4d1e3c319d848e256f2bb36c55993f628c7e7
Lotte Steenbrink <lotte@zombietetris.de>
2024-01-08 22:16:42 +0100

    defmt is defmting

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,16 @@
 #![no_main]
 #![no_std]
 
-
 // Print panic message to probe console
+use defmt_rtt as _;
 use panic_probe as _;
 
-
 use cortex_m_rt::entry;
-use stm32f4xx_hal::{
-    pac,
-    prelude::*,
-};
+use stm32f4xx_hal::{pac, prelude::*};
 
 #[allow(clippy::empty_loop)]
 #[entry]
 fn main() -> ! {
+    defmt::info!("Forstkaestchen up and running 🦌 ");
     loop {}
 }
