ba749c34a1277e8131e169ee42dbac4c87c75e99
Lotte Steenbrink <lotte@zombietetris.de>
2024-09-09 00:49:26 +0200

    blinky for fun

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![no_std]
 
+use cortex_m::peripheral;
 // Print panic message to probe console
 use defmt_rtt as _;
 use panic_probe as _;
@@ -13,5 +14,21 @@ use stm32f4xx_hal::{pac, prelude::*};
 #[entry]
 fn main() -> ! {
     defmt::info!("Forstkaestchen up and running 🦌 ");
-    loop {}
+    let bytes = include_bytes!("test.in"); // <- this should be the .wav file in the future
+    defmt::info!("bytes {}", bytes);
+
+    // blinky for fun
+    let peripherals = pac::Peripherals::take().unwrap();
+    let gpioa = peripherals.GPIOA.split();
+    let mut led = gpioa.pa5.into_push_pull_output();
+
+    loop {
+        for _ in 0..10_000 {
+            led.set_high();
+        }
+
+        for _ in 0..10_000 {
+            led.set_low();
+        }
+    }
 }
