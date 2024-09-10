d1315bbc5a4d9c8373b7ec50aa5b60cb43af061b
Lotte Steenbrink <lotte@zombietetris.de>
2024-09-10 19:03:57 +0200

    convert into rtic structure

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,45 @@
 #![no_main]
 #![no_std]
 
-use cortex_m::peripheral;
 // Print panic message to probe console
 use defmt_rtt as _;
 use panic_probe as _;
 
-use cortex_m_rt::entry;
-use stm32f4xx_hal::{pac, prelude::*};
+#[rtic::app(device = stm32f4xx_hal::pac, dispatchers = [USART1])]
+mod app {
+    use stm32f4xx_hal::gpio::{GpioExt, Output, PA5};
 
-#[allow(clippy::empty_loop)]
-#[entry]
-fn main() -> ! {
-    defmt::info!("Forstkaestchen up and running 🦌 ");
-    let bytes = include_bytes!("test.in"); // <- this should be the .wav file in the future
-    defmt::info!("bytes {}", bytes);
+    #[shared]
+    struct Shared {}
 
-    // blinky for fun
-    let peripherals = pac::Peripherals::take().unwrap();
-    let gpioa = peripherals.GPIOA.split();
-    let mut led = gpioa.pa5.into_push_pull_output();
+    #[local]
+    struct Local {
+        led: PA5<Output>, // only one task uses the LED, so it's local
+    }
 
-    loop {
-        for _ in 0..10_000 {
-            led.set_high();
-        }
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local) {
+        defmt::info!("Forstkaestchen up and running 🦌 ");
+        // let bytes = include_bytes!("test.in"); // <- this should be the .wav file in the future
+        // defmt::info!("bytes {}", bytes);
 
-        for _ in 0..10_000 {
-            led.set_low();
+        // blinky for fun
+        let gpioa = ctx.device.GPIOA.split();
+        let mut led = gpioa.pa5.into_push_pull_output();
+        defmt::info!("led initialized");
+
+        // TODO: make actual use of rtic, rm busy wait :D
+        loop {
+            defmt::info!("toggle high");
+
+            for _ in 0..10_000 {
+                led.set_high();
+            }
+
+            defmt::info!("toggle low");
+            for _ in 0..10_000 {
+                led.set_low();
+            }
         }
     }
 }
