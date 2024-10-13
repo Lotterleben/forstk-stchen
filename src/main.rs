bbd396f4ad262f8a587813a802c68181e23c368d
Lotte Steenbrink <lotte@zombietetris.de>
2024-10-13 23:24:37 +0200

    use rtic tasks for blinky

--- src/main.rs
+++ src/main.rs
@@ -6,41 +6,62 @@
 use defmt_rtt as _;
 use panic_probe as _;
 
+use rtic_monotonics::systick::prelude::*;
+
+//type Mono = stm32f4xx_hal::timer::MonoTimerUs<pac::TIM3>;
+systick_monotonic!(Mono, 1000);
+
 #[rtic::app(device = stm32f4xx_hal::pac, dispatchers = [USART1])]
 mod app {
+    use super::*;
+
+    use rtic_monotonics::fugit::RateExtU32;
     use stm32f4xx_hal::gpio::{GpioExt, Output, PA5};
+    use stm32f4xx_hal::prelude::_stm32f4xx_hal_rcc_RccExt;
+    use stm32f4xx_hal::timer::MonoTimerExt;
 
     #[shared]
     struct Shared {}
 
     #[local]
     struct Local {
-        led: PA5<Output>, // only one task uses the LED, so it's local
+        // this is the "user LD2" led
+        led_ld2: PA5<Output>, // only one task uses the LED, so it's local
     }
 
     #[init]
-    fn init(ctx: init::Context) -> (Shared, Local) {
+    fn init(mut ctx: init::Context) -> (Shared, Local) {
         defmt::info!("Forstkaestchen up and running 🦌 ");
         // let bytes = include_bytes!("test.in"); // <- this should be the .wav file in the future
         // defmt::info!("bytes {}", bytes);
 
+        let rcc = ctx.device.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48_u32.MHz()).freeze();
+
+        // Create TIM3 monotonic and initialize timer queue
+        ctx.device.TIM3.monotonic_us(&mut ctx.core.NVIC, &clocks);
+        Mono::start(ctx.core.SYST, 48_000_000);
+
         // blinky for fun
         let gpioa = ctx.device.GPIOA.split();
-        let mut led = gpioa.pa5.into_push_pull_output();
-        defmt::info!("led initialized");
 
-        // TODO: make actual use of rtic, rm busy wait :D
+        defmt::info!("timer initialized");
+
+        blinky::spawn().ok();
+
+        (
+            Shared {},
+            Local {
+                led_ld2: gpioa.pa5.into_push_pull_output(),
+            },
+        )
+    }
+
+    #[task(local = [led_ld2])]
+    async fn blinky(ctx: blinky::Context) {
         loop {
-            defmt::info!("toggle high");
-
-            for _ in 0..10_000 {
-                led.set_high();
-            }
-
-            defmt::info!("toggle low");
-            for _ in 0..10_000 {
-                led.set_low();
-            }
+            ctx.local.led_ld2.toggle();
+            Mono::delay(500.millis().into()).await;
         }
     }
 }
